5234f7c21b75cba3b2cafcbcb75d37e77622b11c
Yusuf52Ah <yusufxon5225@gmail.com>
2025-08-05 14:21:20 +0500

    2.3 Js

--- main.js
+++ main.js
@@ -4,4 +4,16 @@
 //   return arr.sort((a, b) => a.age - b.age);
 // }
 
+// 2.3 Js
 
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
